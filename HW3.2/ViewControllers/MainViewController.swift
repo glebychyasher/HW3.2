2f6421423b4066113124f75fcc5a38f3f48ebd15
glebychyasher <zobningleb01@icloud.com>
2023-11-13 16:03:52 +0300

    1

--- HW3.2/ViewControllers/MainViewController.swift
+++ HW3.2/ViewControllers/MainViewController.swift
@@ -7,32 +7,64 @@
 
 import UIKit
 
+enum Alert {
+    case success
+    case failed
+    
+    var title: String {
+        switch self {
+        case .success:
+            return "Success"
+        case .failed:
+            return "Failed"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .success:
+            return "You can see the results in the Debug area"
+        case .failed:
+            return "You can see error in the Debug area"
+        }
+    }
+}
+
 final class MainViewController: UIViewController {
     
-    private let url: String = "https://api.carbonintensity.org.uk/generation"
+    private let urlAddress: URL! = URL(string: "https://api.carbonintensity.org.uk/generation")
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func getInfoButtonPressed() {
-        getInfoButtonPressed()
+        getInfoAboutGeneration()
+    }
+    
+    private func showAlert(withStatus status: Alert) {
+        let alert = UIAlertController(title: status.title, message: status.message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        DispatchQueue.main.async { [unowned self] in
+            present(alert, animated: true)
+        }
     }
 }
 
 //MARK: Working with JSON
 extension MainViewController {
     private func getInfoAboutGeneration() {
-        URLSession.shared.dataTask(with: url) { data, _, error in
+        URLSession.shared.dataTask(with: urlAddress) { data, _, error in //необязательно использовать всё
             guard let data = data else {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
-            
             let jsonDecoder = JSONDecoder()
             do {
-                let courses = try jsonDecoder.decode(Data.self, from: data) //передаём тип
+                let info = try jsonDecoder.decode(Data.self, from: data) //передаём тип
                 self.showAlert(withStatus: .success)
+                print(info)
             } catch {
                 print(error.localizedDescription)
                 self.showAlert(withStatus: .failed)
@@ -40,6 +72,6 @@ extension MainViewController {
         }.resume()
         
     }
-    }
 }
+
 
